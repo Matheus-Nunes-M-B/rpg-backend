fa079bd2b073e0bcb817717a1db8829333c4f473
Wand Joseph <55203668+WandJoseph@users.noreply.github.com>
2022-06-04 19:33:33 -0300

    Remove old comments

--- src/users/user/entities/user.entity.ts
+++ src/users/user/entities/user.entity.ts
@@ -30,19 +30,6 @@ export class User {
   @ApiProperty()
   type: UserType;
 
-
-
-  // @Column({ nullable: true })
-  // @ApiProperty()
-  // email: string;
-
-  // @Column({ nullable: true, select: false })
-  // password: string;
-
-  // @Column({ nullable: true, select: false })
-  // @ApiProperty()
-  // description: string;
-
   @CreateDateColumn({ name: 'created_at' })
   @ApiProperty()
   createdAt: Date;
@@ -50,10 +37,4 @@ export class User {
   @UpdateDateColumn({ name: 'updated_at' })
   @ApiProperty()
   updatedAt: Date;
-
-
-  // @BeforeInsert()
-  // async hashPassword() {
-  //   this.password = hashSync(this.password, 10)
-  // }
 }

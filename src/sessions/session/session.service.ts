9f401c841cd4372ad5826cfb6238dbf352b19ded
Matheus <matheusnunesmartinsb@gmail.com>
2022-06-09 20:33:19 -0300

    updated export class

--- src/sessions/session/session.service.ts
+++ src/sessions/session/session.service.ts
@@ -1,26 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BaseService } from 'src/utils/base.service';
+import { Repository } from 'typeorm';
 import { CreateSessionDto } from './dto/create-session.dto';
 import { UpdateSessionDto } from './dto/update-session.dto';
+import { Session } from './entities/session.entity';
 
 @Injectable()
-export class SessionService {
-  create(createSessionDto: CreateSessionDto) {
-    return 'This action adds a new session';
-  }
-
-  findAll() {
-    return `This action returns all session`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} session`;
-  }
-
-  update(id: number, updateSessionDto: UpdateSessionDto) {
-    return `This action updates a #${id} session`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} session`;
+export class SessionService extends BaseService<
+  Session,
+  CreateSessionDto,
+  UpdateSessionDto
+> {
+  constructor(
+    @InjectRepository(Session)
+    private readonly sessionRepository: Repository<Session>,
+  ) {
+    super(sessionRepository);
   }
 }

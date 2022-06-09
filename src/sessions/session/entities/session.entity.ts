4c4ef28ad2b33c7edc55dd3ba1537427b5901a0d
Matheus <matheusnunesmartinsb@gmail.com>
2022-06-08 23:37:50 -0300

    added session entity

--- src/sessions/session/entities/session.entity.ts
+++ src/sessions/session/entities/session.entity.ts
@@ -1,1 +1,39 @@
-export class Session {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Sheet } from 'src/sheets/sheet/entities/sheet.entity';
+import { User } from 'src/users/user/entities/user.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+export enum SessionStatus {
+  ONLINE = 'ONLINE',
+  OFFLINE = 'OFFLINE',
+  COMPLETED = 'COMPLETED',
+  CANCELED = 'CANCELED',
+}
+
+@Entity()
+export class Session {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @ApiProperty()
+  @Column()
+  name: string;
+  @ApiProperty()
+  @Column()
+  status: SessionStatus;
+  @ApiProperty()
+  @Column()
+  background: string;
+  sheets: Sheet[];
+  players: User[];
+  @ManyToOne(() => User, (user) => user.sessions)
+  @JoinColumn({ name: 'master_id' })
+  master: User;
+  @Column({ name: 'master_id' })
+  masterId: number;
+}

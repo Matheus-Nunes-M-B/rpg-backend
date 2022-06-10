3d2310fdfc00fb512a33b6268dd1e7f0996a6607
Wand <caio27joseph@gmail.com>
2022-06-10 20:33:22 -0300

    Added ApiProperty to Status param

--- src/users/user-session/dto/change-user-session-status.params.ts
+++ src/users/user-session/dto/change-user-session-status.params.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum } from 'class-validator';
 import { SessionStatus } from '~/sessions/session/entities/session.entity';
 
 export class ChangeUserSessionStatusParams {
+  @ApiProperty()
   @IsEnum(SessionStatus)
   status: SessionStatus;
 }

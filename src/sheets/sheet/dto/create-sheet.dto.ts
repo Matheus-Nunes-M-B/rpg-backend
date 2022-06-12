f9b09d499edeb7f5af9645e61ad4f082b9b3d451
Wand <caio27joseph@gmail.com>
2022-06-12 15:24:58 -0300

    Add UserSheet Resource

--- src/sheets/sheet/dto/create-sheet.dto.ts
+++ src/sheets/sheet/dto/create-sheet.dto.ts
@@ -43,4 +43,7 @@ export class CreateSheetDto {
 
   @ApiProperty()
   constitution: number;
+
+  @ApiProperty()
+  ownerId: number;
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { UserSessionModule } from './user-session/user-session.module';
+import { UserSheetModule } from './user-sheet/user-sheet.module';
 
 @Module({
-  imports: [UserModule, UserSessionModule],
+  imports: [UserModule, UserSessionModule, UserSheetModule],
 })
 export class UsersModule {}
